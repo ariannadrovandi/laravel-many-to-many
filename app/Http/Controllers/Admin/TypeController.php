f8580e6023279dfe110a054b4c81c063d7470d07
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-06-11 17:57:33 +0200

    added tag model and seeder

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTypeRequest;
 use App\Http\Requests\UpdateTypeRequest;
 use App\Models\Type;
+use app\Models\Project;
 
 class TypeController extends Controller
 {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,10 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            ProjectSeeder::class
+            TypeSeeder::class,
+            ProjectSeeder::class,
+            TagSeeder::class
+
         ]);
     }
 }
