e70355a20b31dfcdcd0f1fbc84e7886778acd53f
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-06-12 01:12:32 +0200

    fix

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -18,14 +18,14 @@
                             <h2>{{ $project->title }}</h2>
                             <p>{{ $project->description }}</p>
                             <p>Type: {{ $project->type ? $project->type->name : 'Senza tipologia' }}</p>
-                            @if ($project->tags && count($post->tags) > 0)
+                            {{-- @if ($project->tags && count($post->tags) > 0)
                                 <div>
                                     @foreach ($project->tags as $tag)
                                         <a href="{{ route('admin.tags.show', $tag->slug) }}"
                                             class="badge rounded-pill text-bg-info">{{ $tag->name }}</a>
                                     @endforeach
                                 </div>
-                            @endif
+                            @endif --}}
                         </div>
                         <div class="d-grid gap-2 d-flex">
                             <button type="button" class="btn me-1 my-2 ms-2 btn-outline-primary">
