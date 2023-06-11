075635bba9dc2840bf67d7b0f1e08f97f943fa5f
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-06-11 12:08:13 +0200

    added files

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+use App\Models\Project;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
+use App\Models\Type;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     *
+     */
+    public function index()
+    {
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     *
+     */
+    public function create(Project $project)
+    {
+        $types = Type::all();
+        return view('admin.projects.create', compact('types'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProjectRequest  $request
+     *
+     */
+    public function store(StoreProjectRequest $request)
+    {
+        $data = $request->validated();
+        $slug = Str::slug($data['title'], '-');
+        $data['slug'] = $slug;
+        $project = Project::create($data);
+        return redirect()->route('admin.projects.show', $project->slug);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     *
+     */
+    public function show(Project $project)
+    {
+        return view('admin.projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     *
+     */
+    public function edit(Project $project)
+    {
+        $types = Type::all();
+        return view('admin.projects.edit', compact('project', 'types'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProjectRequest  $request
+     * @param  \App\Models\Project  $project
+     *
+     */
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        $data = $request->validated();
+        $slug = Str::slug($request->title, '-');
+        $data['slug'] = $slug;
+        $project->update($data);
+        return redirect()->route('admin.projects.show', $project->slug)->with('message', 'Il post modificato con successo.');
+    }
+
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Project  $project
+     *
+     */
+    public function destroy(Project $project)
+    {
+        $project->delete();
+        return redirect()->route('admin.projects.index')->with('message', "Eliminato con successo.");
+    }
+}
---
+++ resources/views/admin/projects/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <h1 class="">Modifica il tuo progetto</h1>
+        <form action="{{ route('admin.projects.update', $project->slug) }}" method="POST">
+            @csrf
+            @method('PUT')
+
+            <div class="mb-3">
+                <label for="title" class="form-label ">Titolo</label>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
+                    aria-describedby="titleHelp" value="{{ $project->title }}">
+                <div id="titleHelp" class="form-text">Modifica il nome</div>
+                @error('title')
+                    <div class="alert alert-danger mt-2">{{ $message }}</div>
+                @enderror
+            </div>
+
+
+            <div class="mb-3">
+                <label for="image" class="form-label ">Immagine</label>
+                <input type="text" class="form-control  @error('image') is-invalid @enderror" name="image"
+                    id="image" aria-describedby="imageHelp" value="{{ $project->image }}">
+                <div id="imageHelp" class="form-text">Modifica l'immagine</div>
+                @error('image')
+                    <div class="alert alert-danger mt-2">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="description" class="form-label ">Descrizione</label>
+                <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="10">
+                </textarea>
+                <div id="descriptionHelp" class="form-text">Modifica la descrizione</div>
+                @error('description')
+                    <div class="alert alert-danger mt-2">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mb3">
+                <label for="type_id">Type</label>
+                <select name="type_id" id="type_id" class="form-control @error('type_id') is-invalid @enderror">
+                    <option value="">Select</option>
+                    @foreach ($types as $type)
+                        <option value="{{ $type->id }}"
+                            {{ $type->id == old('type_id', $project->type_id) ? 'selected' : '' }}>
+                            {{ $type->name }}
+                        </option>
+                    @endforeach
+                </select>
+                @error('category_id')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="reset" class="btn btn-primary">Reset</button>
+        </form>
+    </div>
+@endsection
